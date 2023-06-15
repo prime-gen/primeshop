a8b3d2f083edade2898cc20e743ce0b8370608c2
DanieEscobar <daanielescobar@gmail.com>
2023-06-15 13:15:36 -0600

    web icon and title

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,20 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
   import viteLogo from '/vite.svg'
   import Nav from './lib/Nav.svelte';
   import ProductList from './lib/ProductList.svelte';
   import { Router, Link, Route } from "svelte-routing";
   import Camisa from './lib/Camisa.svelte';
   import Home from './lib/Home.svelte';
-    import Footer from './lib/Footer.svelte';
+  import Footer from './lib/Footer.svelte';
 </script>
 
 
+<svelte:head>
+  <link rel="icon" href="icon.png">
+  <title>PrimeGen Shop</title>
+</svelte:head>
 <Router>
   <main class="h-full relative min-h-full">
-    <!-- <Nav /> -->
-
-    <!-- <div class="
-      bg-no-repeat bg-cover left-0 
-      h-28 sm:h-48 md:h-64 lg:h-[20rem] xl:h-[21rem] 
-      w-screen
-      bg-center 
-      overflow-hidden bg-clip-padding" 
-    style="background-image: url('src/assets/b3.jpg');" >
-    </div> -->
     
     <img src="https://iili.io/H4eeMS1.jpg" class="w-screen" alt="">
 
